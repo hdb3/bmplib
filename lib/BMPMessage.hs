a5c9246b75109747f1c848a2e618934f36543c13
nic hart <nic@nic-Precision-Tower-3420>
2018-10-03 15:55:03 +0100

    first cabalised version, first version which supports the zserv API and streams

--- lib/BMPMessage.hs
+++ lib/BMPMessage.hs
@@ -321,3 +321,21 @@ ipIPv6 = fmap IP.IPv6 ipIPv6Parser
 ipIPv6Parser = do
     v6address <- Data.Attoparsec.ByteString.take 16
     return $ (IP.toIPv6b . map fromIntegral . BS.unpack) v6address
+
+
+
+{-
+   Copyright 2018 Nicholas Hart
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
